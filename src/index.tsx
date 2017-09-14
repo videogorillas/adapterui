bb2d4020e92064c8cd8ecd5a610b7b82713f3494
Anton P. Linevich <anton@linevich.com>
2017-09-13 18:25:44 -0700

    simple pull update for projects

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ProgressBar from 'react-toolbox/lib/progress_bar';
 
 import {Card} from 'react-toolbox/lib/card';
 
-import {Observable, Subject} from 'rx';
+import {CompositeDisposable, Observable, Subject} from 'rx';
 // import * as BigFoot from "bfapi"
 // import {Project} from "bfapi";
 import * as Cloud from "vgcloudapi";
@@ -86,12 +86,14 @@ class ProjectCard extends React.Component<ProjectCardProps, {}> {
             alignItems: "center"
         } as React.CSSProperties;
 
+        let scanscount = this.props.p.scanIds.length;
         return (<Card style={{width: '100%', margin: '0px', float: 'left'}}>
             <table>
                 <tbody>
                 <tr>
                     <td style={colStyle}>
                         {this.props.p.name}<br/>
+                        {this.props.p.status.toString()}
                     </td>
                     <td style={colStyle}>
                         Master<br/>
@@ -100,13 +102,13 @@ class ProjectCard extends React.Component<ProjectCardProps, {}> {
                     </td>
                     <td style={colStyle}>
                         Scans<br/>
-                        1/16<br/>
+                        -1/{scanscount}<br/>
                         <ProgressBar mode='determinate' value={42}/>
                     </td>
                     <td style={colStyle2}>
                         Bigfoot Package<br/>
                         <div style={{float: "left", paddingRight: "5px"}}>Proxy: 3/16</div>
-                        <div style={{float: "left"}}>FDD: 1/16</div>
+                        <div style={{float: "left"}}>FDD: -1/{scanscount}</div>
                         <ProgressBar mode='determinate' value={6.25}/>
                     </td>
                     <td style={colStyle2}>
@@ -122,17 +124,17 @@ class ProjectCard extends React.Component<ProjectCardProps, {}> {
 }
 
 class DashBoardProps {
-    mediaRx: Observable<Media>;
-    projectsRx: Observable<Project>;
+    data: DashBoardData;
 }
 
 class DashBoardState {
-    media: Media[] = [];
+    media: Map<string, Media> = new Map();
     projects: Map<string, Project> = new Map();
 }
 
 
 class BigFootDashboard extends React.Component<DashBoardProps, DashBoardState> {
+    private subs: CompositeDisposable;
 
     constructor(props: DashBoardProps) {
         super(props);
@@ -140,23 +142,23 @@ class BigFootDashboard extends React.Component<DashBoardProps, DashBoardState> {
     }
 
     componentDidMount() {
-        this.props.projectsRx
-            .subscribe(p => {
-                this.setState(prevState => {
-                    if (prevState != null) {
-                        prevState.projects.set(p.id, p);
-                    }
-                })
-            });
+        this.subs = new CompositeDisposable();
+        this.subs.add(this.props.data.projectsRx().subscribe(p => {
+            this.setState(prevState => {
+                if (prevState != null) {
+                    prevState.projects.set(p.id, p);
+                }
+            })
+        }));
 
-        // this.props.mediaRx.subscribe(m => {
-        //     console.log("m=", m);
-        //     this.setState(prevState => {
-        //         if (prevState != null) {
-        //             prevState.media.push(m);
-        //         }
-        //     });
-        // });
+        this.subs.add(this.props.data.mediaRx().subscribe(m => {
+            console.log("m=", m);
+            this.setState(prevState => {
+                if (prevState != null) {
+                    prevState.media.set(m.id, m);
+                }
+            });
+        }));
         // let subscribe = Rx.Observable.interval(420).subscribe(x => this.setState({progress: x}));
     }
 
@@ -179,22 +181,55 @@ class BigFootDashboard extends React.Component<DashBoardProps, DashBoardState> {
 let liveUpdate = new Cloud.LiveUpdate("ws://kote.videogorillas.com:8042/ws/api");
 let bf = new BigFoot.ApiClient("http://kote.videogorillas.com:8042");
 
-function projectsRx() {
-    return bf.listProjectIds()
-        .concatMap(list => {
-            return Observable.from(list);
-        })
-        .concatMap(pId => {
-            return bf.getProjById(pId);
-        });
+
+class DashBoardData {
+    private liveUpdate: Cloud.LiveUpdate;
+    private bf: BigFoot.ApiClient;
+    private subs: CompositeDisposable;
+    private mediaUpdates: Subject<Media>;
+    private projectUpdates: Subject<Project>;
+
+    constructor(liveUpdate: Cloud.LiveUpdate, bf: BigFoot.ApiClient) {
+        this.liveUpdate = liveUpdate;
+        this.bf = bf;
+        this.subs = new CompositeDisposable();
+
+        this.mediaUpdates = new Subject<Media>();
+        this.subs.add(this.liveUpdate.getMediaRx().subscribe(m => {
+            this.mediaUpdates.onNext(m);
+        }));
+
+        this.projectUpdates = new Subject<Project>();
+
+        let projectsO: Observable<Project> = Observable.just(1).merge(Observable.interval(15000))
+            .concatMap(bf.listProjectIds())
+            .concatMap(list => {
+                return Observable.from(list);
+            })
+            .concatMap(pId => {
+                return bf.getProjById(pId);
+            });
+
+        this.subs.add(projectsO.subscribe(p => {
+            this.projectUpdates.onNext(p);
+        }));
+    }
+
+    public mediaRx(): Observable<Media> {
+        return this.mediaUpdates;
+    }
+
+    public projectsRx(): Observable<Project> {
+        return this.projectUpdates;
+    }
+
 }
 
-
+let data = new DashBoardData(liveUpdate, bf);
 console.log("Booting...");
 let ga = document.getElementById("app");
 let p = new DashBoardProps();
-p.mediaRx = liveUpdate.getMediaRx();
-p.projectsRx = projectsRx();
+p.data = data;
 ReactDOM.render(React.createElement(BigFootDashboard, p), ga);
 console.log("OK.");
 
